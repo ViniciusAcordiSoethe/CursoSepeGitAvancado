605158bde605bef5e163b867f86393c8b5b10ecc
ViniciusAcordiSoethe <vinicius.soethe@proton.me>
2023-10-13 00:24:54 -0300

    Feat : Fazendo Algumas alterações para 2023

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "view" , "static" )));
+app.use("/static", express.static(path.resolve(__dirname, "public" , "static" )));
 
 app.get("/*" , (req, res) => {
-   res.sendFile(path.resolve(__dirname , "view" ,  "home.html")); 
+   res.sendFile(path.resolve(__dirname , "public" ,  "home.html")); 
 });
 
 app.listen(process.env.PORT || 3001, () => console.log("Server is running... open http://localhost:3001"));
